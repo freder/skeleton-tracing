cb19d9faa04a64bdd03f786d1128750ac8c0b0df
Frederic Brodbeck <der.froedi@gmail.com>
2021-02-09 12:49:53 +0100

    all the rest

--- swig/example.py
+++ swig/example.py
@@ -2,16 +2,23 @@ import trace_skeleton
 import cv2
 import random
 
-im = cv2.imread("../test_images/opencv-thinning-src-img.png",0)
+for i in range(2000):
+	im = cv2.imread("../test_images/opencv-thinning-src-img.png", 0)
+	_, im = cv2.threshold(im, 128, 255, cv2.THRESH_BINARY)
+	# cv2.imshow("", im);cv2.waitKey(0)
+	polys = trace_skeleton.from_numpy(im)
 
-_,im = cv2.threshold(im,128,255,cv2.THRESH_BINARY);
-# cv2.imshow("",im);cv2.waitKey(0)
+print(polys)
 
-polys = trace_skeleton.from_numpy(im);
+# for line in polys:
+# 	color = (200 * random.random(), 200 * random.random(), 200 * random.random())
+# 	for i in range(0, len(line) - 1):
+# 		cv2.line(
+# 			im,
+# 			(line[i][0], line[i][1]),
+# 			(line[i + 1][0], line[i + 1][1]),
+# 			color
+# 		)
 
-for l in polys:
-	c = (200*random.random(),200*random.random(),200*random.random())
-	for i in range(0,len(l)-1):
-		cv2.line(im,(l[i][0],l[i][1]),(l[i+1][0],l[i+1][1]),c)
-
-cv2.imshow('',im);cv2.waitKey(0)
+# cv2.imshow('', im)
+# cv2.waitKey(0)
